41ac84e5cedcc626ac28193977b693d940397824
mxm1666_thdgit <mehran_mohiuddin@homedepot.com>
2024-10-02 04:15:53 -0400

    adding logic to get spotify bearer token

---
+++ auth.py
@@ -0,0 +1,27 @@
+import requests
+from dotenv import dotenv_values
+import sys
+
+def get_bearer_token():
+    config = dotenv_values(".env")
+
+    url=config["AUTH_URL"]
+    client_id = config["CLIENT_ID"]
+    client_secrets = config["CLIENT_SECRET"]
+
+    request_body = {
+        "grant_type": "client_credentials",
+        "client_id": f"{client_id}",
+        "client_secret": f"{client_secrets}"
+    }
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    response = requests.post(url, data=request_body, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+
+    print("unable to get bearer token. exiting program")
+    sys.exit(1)
+    
